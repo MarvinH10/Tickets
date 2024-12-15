13462fa7c5c0e40cd84cb46423859c49b6dcdb37
Marvin Campos Deza <90712919+MarvinH10@users.noreply.github.com>
2024-12-14 21:01:39 -0500

    Proyecto Finalizado
    Signed-off-by: Marvin Campos Deza <90712919+MarvinH10@users.noreply.github.com>

---
+++ Pages/Admin/Soporte.vue
@@ -0,0 +1,22 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import listSoporte from "@/Components/Admin/listSoporte.vue";
+
+const props = defineProps({
+    success: String,
+});
+
+if (props.success) {
+    setTimeout(() => {
+        window.location.reload();
+    }, 10);
+}
+</script>
+
+<template>
+    <AppLayout v-if="$page.props.auth.user && $page.props.auth.user.rol && $page.props.auth.user.rol.name === 'Admin'" title="Sedes">
+        <div class="py-2">
+            <listSoporte />
+        </div>
+    </AppLayout>
+</template>
